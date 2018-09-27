285ca40e77cd1970073d45155ebe9e8815b01529
Q.Jhone <1148235243@qq.com>
2018-09-27 23:52:00 +0800

    Update cloud_grabber.h

--- cloud_grabber.h
+++ cloud_grabber.h
@@ -9,6 +9,7 @@
 #include <pcl/console/print.h>
 #include <pcl/io/openni2_grabber.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 template <typename PointT>
 class cloudGrabber 
@@ -23,9 +24,13 @@ public:
 		if_viz = flag;
 	};
 public:
+	typename pcl::PointCloud<PointT>::Ptr getSourceCloud();
+	typename pcl::PointCloud<PointT>::Ptr getFilteredCloud();
+	void write2Disk(typename pcl::PointCloud<PointT>::Ptr cloud);
 	void write2Disk(typename pcl::PointCloud<PointT>::ConstPtr cloud);
-	void visualCallback(typename pcl::PointCloud<PointT>::ConstPtr cloud);
-	void processCallback(typename pcl::PointCloud<PointT>::ConstPtr cloud);
+	void visualCloud(typename pcl::PointCloud<PointT>::Ptr cloud);
+	void visualCloud(typename pcl::PointCloud<PointT>::ConstPtr cloud);
+	void filtCloud(typename pcl::PointCloud<PointT>::ConstPtr cloud);
 	void keyboardEventOccurred(const pcl::visualization::KeyboardEvent& event, void* nothing);
 	void run();
 	void stop();
@@ -41,8 +46,11 @@ private:
 	boost::mutex bmutex_;
 	pcl::PCDWriter writer_;
 	pcl::io::OpenNI2Grabber grabber_;
-	typename pcl::PointCloud<PointT>::Ptr cloud_;
-	boost::shared_ptr<pcl::visualization::CloudViewer> viewer_;	
+	typename pcl::PointCloud<PointT>::Ptr cloud_ = nullptr;
+	typename pcl::PointCloud<PointT>::Ptr cloud_filtered = nullptr;
+	typename pcl::StatisticalOutlierRemoval<PointT> sor;
+	boost::shared_ptr<pcl::visualization::CloudViewer> viewer_src_cloud;
+	boost::shared_ptr<pcl::visualization::CloudViewer> viewer_filtered_cloud;
 };
 
 template<typename PointT> cloudGrabber<PointT>* cloudGrabber<PointT>::m_instance= nullptr;
