99b516c4bcce7de1cf4169e3225ad84fa108d526
Q.Jhone <1148235243@qq.com>
2018-10-13 13:28:31 +0800

    Update kinnect_grabber.h

--- io/kinnect_grabber.h
+++ io/kinnect_grabber.h
@@ -2,6 +2,7 @@
 #ifndef  MY_POINT_CLOUD_GRABBER_H
 #define MY_POINT_CLOUD_GRABBER_H
 
+#include "basic_func.h"
 #include <sstream>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
@@ -13,88 +14,72 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 
-#define FPS_CALC(_WHAT_) \
-do \
-{ \
-    static unsigned count = 0;\
-    static double last = pcl::getTime ();\
-    double now = pcl::getTime (); \
-    ++count; \
-    if (now - last >= 1.0) \
-    { \
-      std::cout << "Average framerate("<< _WHAT_ << "): " << double(count)/double(now - last) << " Hz" <<  std::endl; \
-      count = 0; \
-      last = now; \
-    } \
-}while(false)
 
 template <typename PointT>
-class cloudGrabber 
+class kinnectGrabber 
 {
 public:
-	inline bool setSaveFlag(bool flag)
+	kinnectGrabber(pcl::io::OpenNI2Grabber& grabber)
+		: grabber_(grabber)
+		, cloud_viewer_(new pcl::visualization::PCLVisualizer("PCL OpenNI2 cloud"))
+		, rgb_viewer_()
+		, depth_viewer_()
+	{
+	};
+	inline void setSaveFlag(bool flag)
 	{
 		if_save_cloud = flag;
 	};
-	inline bool setVizCloudFlag(bool flag)
+	inline void setVizCloudFlag(bool flag)
 	{
 		if_viz_depth = flag;
 	};
-	inline bool setVizRGBFlag(bool flag)
+	inline void setVizRGBFlag(bool flag)
 	{
 		if_viz_rgb = flag;
 	};
-	inline bool setVizDepthFlag(bool flag)
+	inline void setVizDepthFlag(bool flag)
 	{
 		if_viz_cloud = flag;
 	};
 public:
-	void run();
-	void stop();
-	static cloudGrabber* getInstance();
-	typename pcl::PointCloud<PointT>::ConstPtr getLatestCloud();	
-	boost::shared_ptr<pcl::io::Image> getLatestRGBImg();
-	boost::shared_ptr<pcl::io::DepthImage> getLatestDepthImg();	
+	void run(void);
+	typename pcl::PointCloud<PointT>::ConstPtr getLatestCloud(void);	
+	boost::shared_ptr<pcl::io::openni2::Image> getLatestRGBImg(void);
+	boost::shared_ptr<pcl::io::openni2::DepthImage> getLatestDepthImg(void);	
 	void saveCLoud2Disk();
 	void saveRGBImg2Disk(const std::string& file_name);
 	void saveDepthImg2Disk(const std::string& file_name);
 private:
-	cloudGrabber()
-		: cloud_viewer_("PCL OpenNI Viewer")
-		, rgb_viewer_("PCL rgb image viewer")
-		, depth_viewer_("PCL depth image viewer")
-	{
-	};
-	cloudGrabber(const cloudGrabber&);
-	cloudGrabber& operator=(const cloudGrabber&);
+	kinnectGrabber(const kinnectGrabber&);
+	kinnectGrabber& operator=(const kinnectGrabber&);
 	void cloud_callback(typename const pcl::PointCloud<PointT>::ConstPtr cloud);
 	void rgb_image_callback(const boost::shared_ptr<pcl::io::Image>& rgb);
 	void depth_image_callback(const boost::shared_ptr<pcl::io::DepthImage>& depth);
 	void mouse_callback(const pcl::visualization::MouseEvent& mouse_event, void* cookie);
 	void keyboard_callback(const pcl::visualization::KeyboardEvent& event, void* cookie);
 private:
-	boost::mutex rgb_mutex_;
+	pcl::PCDWriter writer_;
+	pcl::io::OpenNI2Grabber& grabber_;
+private:
 	boost::mutex cloud_mutex_;
-	boost::mutex depth_mutex_;
-	static cloudGrabber* m_instance;
-private:
-	bool if_viz_rgb = true;
-	bool if_viz_depth = true;
-	bool if_viz_cloud = true;
-	bool if_save_rgb = false;
-	bool if_save_depth = false;
 	bool if_save_cloud = false;
-private:
-	pcl::PCDWriter writer_ = new pcl::PCDWriter writer;
-	pcl::io::OpenNI2Grabber grabber_ = new pcl::io::OpenNI2Grabber grabber;
-	pcl::visualization::ImageViewer rgb_viewer_;
-	pcl::visualization::ImageViewer depth_viewer_;
-	pcl::visualization::CloudViewer cloud_viewer_;
+	bool if_viz_cloud = true;
 	typename pcl::PointCloud<PointT>::ConstPtr cloud_ = nullptr;
-	boost::shared_ptr<pcl::io::Image> rgb_image_ = nullptr;
-	boost::shared_ptr<pcl::io::DepthImage> depth_image_ = nullptr;
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> cloud_viewer_;
+private:
+	boost::mutex rgb_mutex_;
+	bool if_viz_rgb = true;
+	bool if_save_rgb = false;
+	boost::shared_ptr<pcl::io::openni2::Image> rgb_image_ = nullptr;
+	boost::shared_ptr<pcl::visualization::ImageViewer> rgb_viewer_;
+private:
+	boost::mutex depth_mutex_;	
+	bool if_viz_depth = true;
+	bool if_save_depth = false;
+	boost::shared_ptr<pcl::io::openni2::DepthImage> depth_image_ = nullptr;
+	boost::shared_ptr<pcl::visualization::ImageViewer> depth_viewer_;
 };
 
-template<typename PointT> cloudGrabber<PointT>* cloudGrabber<PointT>::m_instance= nullptr;
 
 #endif // ! MY_POINT_CLOUD_GRABBER_H
