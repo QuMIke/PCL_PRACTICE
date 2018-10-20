a06c1bdbc1709aafb7de15d6534d411f722f4931
Q.Jhone <1148235243@qq.com>
2018-10-20 12:48:31 +0800

    Create ExtractIndices .cpp

---
+++ filter/ExtractIndices .cpp
@@ -0,0 +1,4 @@
+/*
+In this tutorial we will learn how to use an pcl::ExtractIndices <pcl::ExtractIndices> filter
+to extract a subset of points from a point cloud based on the indices output by a segmentation algorithm.
+*/
