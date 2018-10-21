2b880868c95ef3c6b57a2177b5709082cac53de7
Q.Jhone <1148235243@qq.com>
2018-10-21 10:01:18 +0800

    Create conditional_removal.cpp

---
+++ filter/conditional_removal.cpp
@@ -0,0 +1,7 @@
+/*
+This document demonstrates how to remove outliers from a PointCloud 
+using several different methods in the filter module. 
+we will look at how to use a ConditionalRemoval filter 
+which removes all indices in the given input cloud that do not satisfy one or more given conditions.
+*/
+
