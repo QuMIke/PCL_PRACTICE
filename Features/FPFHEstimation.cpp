28d9799f9dd6a1361215c2b7830592e4ffc1b326
Q.Jhone <1148235243@qq.com>
2018-10-25 21:28:57 +0800

    Create FPFHEstimation.cpp

---
+++ Features/FPFHEstimation.cpp
@@ -0,0 +1,7 @@
+/*
+For real-time or near real-time applications, the computation of 
+Point Feature Histograms in dense point neighborhoods can represent one of the major bottlenecks.
+This tutorial describes a simplification of the PFH formulation, 
+called Fast Point Feature Histograms (FPFH), that reduces the computational complexity of the algorithm to O(nk), 
+while still retaining most of the discriminative power of the PFH.
+*/
