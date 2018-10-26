b02178582a6aa3f13a43e1a849e13059d26901ed
Q.Jhone <1148235243@qq.com>
2018-10-26 22:54:26 +0800

    Update FPFHEstimation.cpp

--- Features/FPFHEstimation.cpp
+++ Features/FPFHEstimation.cpp
@@ -5,3 +5,54 @@ This tutorial describes a simplification of the PFH formulation,
 called Fast Point Feature Histograms (FPFH), that reduces the computational complexity of the algorithm to O(nk), 
 while still retaining most of the discriminative power of the PFH.
 */
+
+#include "stdafx.h"
+#include <iostream>
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/fpfh.h>
+#include "KinnectGrabber.h"
+
+int main()
+{
+	pcl::io::OpenNI2Grabber grabber;
+	KinnectGrabber<pcl::PointXYZRGBA> v(grabber);
+	v.run();
+
+	pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud = nullptr;
+	cloud = v.getLatestCloud();
+	if (cloud == nullptr)
+	{
+		std::cout << "Get cloud failed!" << std::endl;
+	}
+	else
+	{
+		// create output normals
+		pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>());
+
+		// Create the FPFH estimation class, and pass the input dataset+normals to it
+		pcl::FPFHEstimation<pcl::PointXYZRGBA, pcl::Normal, pcl::FPFHSignature33> fpfh;
+		fpfh.setInputCloud(cloud);
+		fpfh.setInputNormals(normals);
+		// alternatively, if cloud is of tpe PointNormal, do fpfh.setInputNormals (cloud);
+
+		// Create an empty kdtree representation, and pass it to the FPFH estimation object.
+		// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+		pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
+
+		fpfh.setSearchMethod(tree);
+
+		// Output datasets
+		pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs(new pcl::PointCloud<pcl::FPFHSignature33>());
+
+		// Use all neighbors in a sphere of radius 5cm
+		// IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
+		fpfh.setRadiusSearch(0.05);
+
+		// Compute the features
+		fpfh.compute(*fpfhs);
+
+	}
+
+	return 0;
+}
