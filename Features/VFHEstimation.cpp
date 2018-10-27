48db49b53239ce3d7aaa1569a9ff4dbf417167cd
Q.Jhone <1148235243@qq.com>
2018-10-27 11:13:52 +0800

    Update VFHEstimation.cpp

--- Features/VFHEstimation.cpp
+++ Features/VFHEstimation.cpp
@@ -19,18 +19,33 @@ int main()
 	KinnectGrabber<pcl::PointXYZRGBA> v(grabber);
 	v.run();
 
-	pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud = nullptr;
+	pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
 	cloud = v.getLatestCloud();
+
 	if (cloud == nullptr)
 	{
 		std::cout << "Get cloud failed!" << std::endl;
 	}
 	else
 	{
-		// create output normals
-		pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>());
+		// create normal estimator
+		pcl::NormalEstimation<pcl::PointXYZRGBA, pcl::Normal> ne;
+		ne.setInputCloud(cloud);
 
-		// Create the FPFH estimation class, and pass the input dataset+normals to it
+		// create kdtree for search
+		pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree_n(new pcl::search::KdTree<pcl::PointXYZRGBA>());
+		ne.setSearchMethod(tree_n);
+
+		// output normals
+		pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+
+		// using all neighbors in a sphere of radius 3cm
+		ne.setRadiusSearch(0.03);
+
+		// compute normals
+		ne.compute(*normals);
+
+		// Create the VFH estimation class, and pass the input dataset+normals to it
 		pcl::VFHEstimation<pcl::PointXYZRGBA, pcl::Normal, pcl::VFHSignature308> vfh_e;
 		vfh_e.setInputCloud(cloud);
 		vfh_e.setInputNormals(normals);
@@ -38,12 +53,12 @@ int main()
 
 		// Create an empty kdtree representation, and pass it to the FPFH estimation object.
 		// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-		pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
-		vfh_e.setSearchMethod(tree);
+		pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree_f(new pcl::search::KdTree<pcl::PointXYZRGBA>);
+		vfh_e.setSearchMethod(tree_f);
 
 		// Use all neighbors in a sphere of radius 5cm
 		// IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
-		vfh_e.setRadiusSearch(0.05);
+		vfh_e.setRadiusSearch(0.02);
 
 		// Output datasets
 		pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs(new pcl::PointCloud<pcl::VFHSignature308>());
