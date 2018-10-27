0dac9ea27f50fb23f77ee236600d1af0ed087eb4
Q.Jhone <1148235243@qq.com>
2018-10-27 10:23:58 +0800

    Create VFHEstimation.cpp

---
+++ Features/VFHEstimation.cpp
@@ -0,0 +1,8 @@
+/*
+This document describes the Viewpoint Feature Histogram ([VFH]) descriptor, 
+a novel representation for point clusters for the problem of Cluster (e.g., Object) Recognition and 6DOF Pose Estimation.
+The Viewpoint Feature Histogram (or VFH) has its roots in the FPFH descriptor (see Fast Point Feature Histograms (FPFH) descriptors).
+Due to its speed and discriminative power, we decided to leverage the strong recognition results of FPFH, 
+but to add in viewpoint variance while retaining invariance to scale.
+*/
+
