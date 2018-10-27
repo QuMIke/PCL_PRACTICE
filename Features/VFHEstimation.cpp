090da6ad950e61d4e347f5a88ee0818d6f456a71
Q.Jhone <1148235243@qq.com>
2018-10-27 10:29:01 +0800

    Update VFHEstimation.cpp

--- Features/VFHEstimation.cpp
+++ Features/VFHEstimation.cpp
@@ -6,3 +6,52 @@ Due to its speed and discriminative power, we decided to leverage the strong rec
 but to add in viewpoint variance while retaining invariance to scale.
 */
 
+#include "stdafx.h"
+#include <iostream>
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/vfh.h>
+#include "KinnectGrabber.h"
+
+int main()
+{
+	pcl::io::OpenNI2Grabber grabber;
+	KinnectGrabber<pcl::PointXYZRGBA> v(grabber);
+	v.run();
+
+	pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud = nullptr;
+	cloud = v.getLatestCloud();
+	if (cloud == nullptr)
+	{
+		std::cout << "Get cloud failed!" << std::endl;
+	}
+	else
+	{
+		// create output normals
+		pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>());
+
+		// Create the FPFH estimation class, and pass the input dataset+normals to it
+		pcl::VFHEstimation<pcl::PointXYZRGBA, pcl::Normal, pcl::VFHSignature308> vfh_e;
+		vfh_e.setInputCloud(cloud);
+		vfh_e.setInputNormals(normals);
+		// alternatively, if cloud is of tpe PointNormal, do fpfh.setInputNormals (cloud);
+
+		// Create an empty kdtree representation, and pass it to the FPFH estimation object.
+		// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+		pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
+		vfh_e.setSearchMethod(tree);
+
+		// Use all neighbors in a sphere of radius 5cm
+		// IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
+		vfh_e.setRadiusSearch(0.05);
+
+		// Output datasets
+		pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs(new pcl::PointCloud<pcl::VFHSignature308>());
+		
+		// Compute the features
+		vfh_e.compute(*vfhs);
+
+	}
+
+	return 0;
+}
