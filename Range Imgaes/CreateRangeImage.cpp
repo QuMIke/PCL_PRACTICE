efd257bcf7d6f537a1f6af88d340f9d14cf082e8
Q.Jhone <1148235243@qq.com>
2018-10-27 11:55:58 +0800

    Create CreateRangeImage.cpp

---
+++ Range Imgaes/CreateRangeImage.cpp
@@ -0,0 +1,6 @@
+/*
+This tutorial demonstrates how to create a range image from a point cloud and a given sensor position. 
+What is range image? It's main idea is that the three-dimensional point cloud is projected directly into two dimensional images.
+We call this projection is  range_image.
+*/
+
