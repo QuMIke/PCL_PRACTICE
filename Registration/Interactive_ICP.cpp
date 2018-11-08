6542ae92fe622dd1ebd18957475d2687a8b65afc
Q.Jhone <1148235243@qq.com>
2018-11-08 19:32:45 +0800

    Create Interactive_ICP.cpp

---
+++ Registration/Interactive_ICP.cpp
@@ -0,0 +1,6 @@
+/*
+This tutorial will teach you how to write an interactive ICP viewer.
+The program will load a point cloud and apply a rigid transformation on it.
+After that the ICP algorithm will align the transformed point cloud with the original. 
+Each time the user presses “space” an ICP iteration is done and the viewer is refreshed.
+*/
